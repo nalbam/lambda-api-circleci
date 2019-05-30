418e46ec984f642166aab1f76e7e732de5d12535
nalbam <me@nalbam.com>
2019-05-30 23:09:40 +0900

    terraform 0.12

--- variables.tf
+++ variables.tf
@@ -1,31 +1,27 @@
-variable region {
+variable "region" {
   default = "ap-northeast-2"
 }
 
-variable name {
-  default = "docker-to-circlci"
+variable "name" {
+  default = "api-circlci"
 }
 
-variable stage {
+variable "stage" {
   default = "dev"
 }
 
-variable domain {
+variable "domain" {
   default = "nalbam.com"
 }
 
-variable s3_bucket {
+variable "s3_bucket" {
   default = "repo.nalbam.com"
 }
 
-variable version {
-  default = "1.0.0"
-}
-
-variable CIRCLECI_API {
+variable "CIRCLECI_API" {
   default = "https://circleci.com/api/v1.1/project/github/username/project"
 }
 
-variable CIRCLECI_TOKEN {
+variable "CIRCLECI_TOKEN" {
   default = ""
 }
